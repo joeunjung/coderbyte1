0978661aaa7433c3973fa21f5fdd1b97e6b0dca3
joeunjung <joeunjung@users.noreply.github.com>
2015-07-30 16:38:10 +0900

    Create longest_word.rb

---
+++ longest_word.rb
@@ -0,0 +1,9 @@
+def LongestWord(sen)
+
+  return sen.split(" ").map { |s| s.gsub(/\W+/,"") }.max_by(&:length)
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+LongestWord(STDIN.gets)   
