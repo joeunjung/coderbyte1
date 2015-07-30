a84bb9b4fa354a8916f32b97e0d0c6c808971ce9
joeunjung <joeunjung@users.noreply.github.com>
2015-07-31 00:35:14 +0900

    Create Number Addition.rb

---
+++ Number Addition.rb
@@ -0,0 +1,11 @@
+def NumberAddition(str)
+
+  # code goes here
+  str = str.split(/[^0-9]/) 
+  str.inject(0){|sum , x| sum + x.to_i}
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+NumberAddition(STDIN.gets)  
